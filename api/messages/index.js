77d68b875a447152a201f0b476d0bc2868ebdaf2
Penny4Thought708 <Haroldgore40@gmail.com>
2026-01-18 01:19:00 -0500

    Create index.js

---
+++ api/messages/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import auth from "../../middleware/authMiddleware.js";
+
+import send from "./send.js";
+import thread from "./thread.js";
+import markRead from "./markRead.js";
+import markDelivered from "./markDelivered.js";
+import hide from "./hide.js";
+import restore from "./restore.js";
+import deleteMsg from "./delete.js";
+import hidden from "./hidden.js";
+import react from "./react.js";
+import edit from "./edit.js";
+import audio from "./audio.js";
+import upload from "./upload.js";
+
+const router = express.Router();
+
+router.use(auth);
+
+router.post("/send", send);
+router.get("/thread/:contactId", thread);
+router.post("/mark-read", markRead);
+router.post("/mark-delivered", markDelivered);
+router.post("/hide", hide);
+router.post("/restore", restore);
+router.post("/delete", deleteMsg);
+router.get("/hidden", hidden);
+router.post("/react", react);
+router.post("/edit", edit);
+router.post("/audio", audio);
+router.post("/upload", upload);
+
+export default router;
