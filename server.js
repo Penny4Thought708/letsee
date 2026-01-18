d53bd7eb222f8c6b0db037ee2d7c8b0aff6752ff
Penny4Thought708 <Haroldgore40@gmail.com>
2026-01-18 05:03:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import cookieParser from "cookie-parser";
 import fs from "fs";
 import WaveformData from "waveform-data";
 import registerWebRTCHandlers from "./sockets/webrtc.js";
+import authMiddleware from "../../middleware/authMiddleware.js";
 
 
 
@@ -640,6 +641,7 @@ const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
 
 
