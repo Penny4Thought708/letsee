a9f762eae830ce734a51cf675c127a9a19fbe845
Penny4Thought708 <Haroldgore40@gmail.com>
2026-01-19 15:16:45 -0500

    Delete sockets directory

--- sockets/reactions.js
+++
@@ -1,9 +0,0 @@
-// node-backend/sockets/reactions.js
-
-export default function registerReactionHandlers(io, socket) {
-  // When a client toggles a reaction, broadcast the result
-  socket.on("reaction:toggle", (payload) => {
-    // payload typically: { message_id, user_id, emoji, ... }
-    io.emit("reaction:update", payload);
-  });
-}
