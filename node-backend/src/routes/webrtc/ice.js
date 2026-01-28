bcc9b84440f4341ccdb259aeadc9e5dc4fbdde9c
Penny4Thought708 <Haroldgore40@gmail.com>
2026-01-28 17:20:34 -0500

    Update ice.js

--- node-backend/src/routes/webrtc/ice.js
+++ node-backend/src/routes/webrtc/ice.js
@@ -5,9 +5,20 @@ const router = express.Router();
 router.get("/get-ice", (req, res) => {
   res.json({
     iceServers: [
-      { urls: "stun:stun.l.google.com:19302" }
+      {
+        urls: [
+          "turns:your-turn-domain:443?transport=tcp",
+          "turn:your-turn-domain:3478?transport=udp"
+        ],
+        username: "yourTurnUsername",
+        credential: "yourTurnPassword"
+      },
+      {
+        urls: ["stun:stun.l.google.com:19302"]
+      }
     ]
   });
 });
+
 
 export default router;
