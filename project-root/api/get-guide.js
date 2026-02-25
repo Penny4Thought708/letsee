7eb71a4e0a872ba1f9169b29d992f49e9805cf3d
Penny4Thought708 <Haroldgore40@gmail.com>
2026-02-25 06:14:06 -0500

    Update get-guide.js

--- project-root/api/get-guide.js
+++ project-root/api/get-guide.js
@@ -1,19 +1,23 @@
 import fs from "fs";
 import path from "path";
 import express from "express";
+
 const router = express.Router();
 
-router.get("/:slug", (req, res) => {
-  const slug = req.params.slug;
-  const filePath = path.join("public/guides", `${slug}.html`);
+router.get("/:slug", (req, res, next) => {
+  try {
+    const slug = req.params.slug;
+    const filePath = path.join("public", "guides", `${slug}.html`);
 
-  if (!fs.existsSync(filePath)) {
-    return res.status(404).json({ error: "Guide not found" });
+    if (!fs.existsSync(filePath)) {
+      return res.status(404).json({ error: "Guide not found" });
+    }
+
+    const html = fs.readFileSync(filePath, "utf8");
+    res.type("html").send(html);
+  } catch (err) {
+    next(err);
   }
-
-  const html = fs.readFileSync(filePath, "utf8");
-  res.send(html);
 });
 
 export default router;
-// 
